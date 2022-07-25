e5bdb6f2998a18c5fd19f9e159b6f827a446dc88
mancamar <hiis.isti@gmail.com>
2022-07-25 12:58:07 +0200

    added local docs and page eval data

--- src/main/java/it/cnr/isti/hiis/largescalevalidationreadjsondata/LargeScaleValidationReadJsonData.java
+++ src/main/java/it/cnr/isti/hiis/largescalevalidationreadjsondata/LargeScaleValidationReadJsonData.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import largescalevalidation.model.mongo.MongoEvalData;
+import largescalevalidation.model.mongo.MongoEvalPageResult;
 
 /**
  *
@@ -17,15 +18,20 @@ public class LargeScaleValidationReadJsonData {
 
     public static void main(String[] args) {
         try {
-            File summaryFile = new File("C:\\Users\\Marco Manca\\Documents\\CNR Manca\\my_doc\\Progetti\\Agid\\2022-07-19-export\\eval_summary_2022-07-25-12-02.json");
+            File summaryFile = new File("docs\\eval_summary_2022-07-25-12-02.json");
             ObjectMapper mapper = new ObjectMapper();
             mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);            
             List<MongoEvalData> evalDataList = mapper.readValue(summaryFile, 
                     mapper.getTypeFactory().constructCollectionType(List.class, MongoEvalData.class));
             for(MongoEvalData _evalData : evalDataList) {
-                System.out.println("IpaCode ".concat(_evalData.getIpaCode()));
+                System.out.println("IpaCode ".concat(_evalData.getIpaCode()));                                
             }
-            
+            File pageEvalFile = new File("docs\\page_eval_2022-07-25-12-48.json");
+            List<MongoEvalPageResult> pageEvalList = mapper.readValue(pageEvalFile, 
+                    mapper.getTypeFactory().constructCollectionType(List.class, MongoEvalPageResult.class));
+            for(MongoEvalPageResult res : pageEvalList) {
+                System.out.println("URL ".concat(res.getUrl()));
+            }
         } catch (IOException ex) {
             Logger.getLogger(LargeScaleValidationReadJsonData.class.getName()).log(Level.SEVERE, null, ex);
         }
